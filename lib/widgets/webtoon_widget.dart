0e65aa3eddd03afa7e0d11363efc704e0d2d6733
Heungseop <imt300@naver.com>
2023-05-09 15:43:29 +0900

    req header global override

--- lib/widgets/webtoon_widget.dart
+++ lib/widgets/webtoon_widget.dart
@@ -41,10 +41,6 @@ class Webtoon extends StatelessWidget {
                 ]),
             child: Image.network(
               thumb,
-              headers: const {
-                "User-Agent":
-                    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-              },
             ),
           ),
           const SizedBox(
