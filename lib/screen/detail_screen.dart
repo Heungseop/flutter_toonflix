084198fbb8b5eea2e6791a4b23a94d697c9488b4
Heungseop <imt300@naver.com>
2023-05-10 11:19:45 +0900

    6.17 Favorites

--- lib/screen/detail_screen.dart
+++ lib/screen/detail_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_toonflix/models/webrtoon_detail_model.dart';
 import 'package:flutter_toonflix/models/webrtoon_episode_model.dart';
 import 'package:flutter_toonflix/widgets/episode_widget.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../services/api_service.dart';
 
@@ -22,6 +23,20 @@ class DetailScreen extends StatefulWidget {
 class _DetailScreenState extends State<DetailScreen> {
   late Future<WebToonDetailModel> webtoon;
   late Future<List<WebToonEpisodeModel>> epList;
+  late SharedPreferences prefs;
+  bool isLike = false;
+
+  Future initPref() async {
+    prefs = await SharedPreferences.getInstance();
+    final likedToons = prefs.getStringList("likedToons");
+    if (likedToons != null) {
+      setState(() {
+        isLike = likedToons.contains(widget.id);
+      });
+    } else {
+      prefs.setStringList("likedToons", []);
+    }
+  }
 
   // 초기화하고 싶은 멤버가 있는데 생성자에서 못하는 경우 initState에서 작업한다.
   @override
@@ -29,6 +44,22 @@ class _DetailScreenState extends State<DetailScreen> {
     super.initState();
     webtoon = ApiService.getToonById(widget.id);
     epList = ApiService.getLatestEpisodeById(widget.id);
+    initPref();
+  }
+
+  onHeartTap() async {
+    var likedToons = prefs.getStringList("likedToons");
+
+    if (likedToons != null) {
+      var set = Set<String>.from(likedToons);
+      likedToons = set.toList();
+      setState(() {
+        isLike ? likedToons!.remove(widget.id) : likedToons!.add(widget.id);
+        isLike = !isLike;
+      });
+      print("$isLike, $likedToons");
+      await prefs.setStringList("likedToons", likedToons);
+    }
   }
 
   @override
@@ -40,6 +71,14 @@ class _DetailScreenState extends State<DetailScreen> {
         backgroundColor: Colors.white,
         foregroundColor: Colors.green,
         centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: onHeartTap,
+            icon: isLike
+                ? const Icon(Icons.favorite_outlined)
+                : const Icon(Icons.favorite_outline_outlined),
+          )
+        ],
         title: Text(
           widget.title,
           style: const TextStyle(
